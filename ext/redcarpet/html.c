7c1069c96bad744e80ea683239ac4a1f76e8d314
YOSHIOKA Eiichiro <yoshioka.eiichiro@gmail.com>
2017-01-19 21:36:57 +0900

    Update html.c

--- ext/redcarpet/html.c
+++ ext/redcarpet/html.c
@@ -491,7 +491,7 @@ rndr_image(struct buf *ob, const struct buf *link, const struct buf *title, cons
 	if (link != NULL && (options->flags & HTML_SAFELINK) != 0 && !sd_autolink_issafe(link->data, link->size))
 		return 0;
 
-	BUFPUTSL(ob, "<img src=\"");
+	BUFPUTSL(ob, "<img class=\"mg-responsive" src=\"");
 
 	if (link && link->size)
 		escape_href(ob, link->data, link->size);
